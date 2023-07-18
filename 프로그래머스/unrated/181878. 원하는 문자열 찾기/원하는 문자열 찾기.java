b516e3295794763acb16d61d1506261d8cdcf851
KimDoHoon4119 <124942682+KimDoHoon4119@users.noreply.github.com>
2023-07-19 00:45:13 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 8.36 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        if(myString.toLowerCase().contains(pat.toLowerCase())) {
+            return 1;
+		} else {
+            return 0;
+        }
+    }
+}
